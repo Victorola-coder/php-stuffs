f6603e2f21e8b6bbd79a32060ae8e2ae752e7411
Victor Olatunji <victoluolatunji@gmail.com>
2024-03-06 19:27:24 +0100

    [chore]: task 6

---
+++ tasks/06-redirect/index.php
@@ -0,0 +1,13 @@
+<?php
+// task6.php
+
+// Function to redirect the user to a different page
+function redirectToPage($page) {
+    // Use header() function to redirect
+    header("Location: $page");
+    exit; // Ensure that no further code is executed after the redirect
+}
+
+// Example usage: redirect to Google's homepage
+redirectToPage("https://www.google.com");
+?>
