e920f86d4d3df2e5ae25a04d430a0629bf1084ab
Victor Olatunji <victoluolatunji@gmail.com>
2024-03-06 19:06:33 +0100

    .

--- index.php
+++ index.php
@@ -1,4 +1,17 @@
-<?php
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <title>Welcome</title>
+    </head>
 
- echo "hello world"
-?>
+    <body>
+        
+        <?php
+
+        echo "hello world"
+        ?>
+    
+    </body>
+</html>
